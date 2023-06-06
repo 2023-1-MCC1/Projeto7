443047693990440951da2d616741734bd25696d6
Lucca Giordano <giordanolucca871@gmail.com>
2023-06-06 13:26:47 -0300

    bug ao reiniciar o jogo concertado

--- scripts/TelaDerrota.cs
+++ scripts/TelaDerrota.cs
@@ -3,20 +3,26 @@ using System;
 
 public partial class TelaDerrota : Node2D
 {
+
+	game game = new game();
+
 	private void _on_button_reiniciar_pressed()
 	{
+		Array.Clear(game.descobertas, 0, game.descobertas.Length);
 		GetTree().ChangeSceneToFile("res://scenes/JogoPrincipal.tscn");
 	}
 
 
 	private void _on_button_voltar_inicio_pressed()
 	{
+		Array.Clear(game.descobertas, 0, game.descobertas.Length);
 		GetTree().ChangeSceneToFile("res://scenes/TelaInicial.tscn");
 	}
 
 
 	private void _on_button_sair_pressed()
 	{
+		Array.Clear(game.descobertas, 0, game.descobertas.Length);
 		GetTree().Quit();
 	}
 }
